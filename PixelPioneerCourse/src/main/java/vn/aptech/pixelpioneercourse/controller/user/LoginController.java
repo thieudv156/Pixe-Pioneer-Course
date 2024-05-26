42c2fdb5661dd890200a9de0544c50503e75bb0e
NAM KHANH <khanhvdnts2207031@fpt.edu.vn>
2024-05-26 18:11:36 +0700

    LOGIN UI + FEATURES UPDATED

--- PixelPioneerCourse/src/main/java/vn/aptech/pixelpioneercourse/controller/user/LoginController.java
+++ PixelPioneerCourse/src/main/java/vn/aptech/pixelpioneercourse/controller/user/LoginController.java
@@ -1,12 +1,32 @@
 package vn.aptech.pixelpioneercourse.controller.user;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import vn.aptech.pixelpioneercourse.service.AccountService;
 
 @Controller
-@RequestMapping("/login")
+@RequestMapping("")
 public class LoginController {
+
+    @Autowired
+    private AccountService accountService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
     @GetMapping("")
-    public String index() {return "login";}
+    public String index() {return "/login";}
+
+    @PostMapping("/logintoaccount")
+    public String checkLogin(String email, String password) {
+        if (accountService.checkLogin(email,password)) {
+            return "redirect:/course";
+        } else {
+            return "redirect:/login";
+        }
+    }
 }
