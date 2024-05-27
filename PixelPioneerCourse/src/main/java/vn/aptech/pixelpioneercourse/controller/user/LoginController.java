cdf7e3df8a1810d94e395e126c9b53e43c4d8bf4
NAM KHANH <khanhvdnts2207031@fpt.edu.vn>
2024-05-27 18:02:19 +0700

    LOGIN UPDATE

--- PixelPioneerCourse/src/main/java/vn/aptech/pixelpioneercourse/controller/user/LoginController.java
+++ PixelPioneerCourse/src/main/java/vn/aptech/pixelpioneercourse/controller/user/LoginController.java
@@ -1,6 +1,5 @@
 package vn.aptech.pixelpioneercourse.controller.user;
 
-<<<<<<< Updated upstream
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,8 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import vn.aptech.pixelpioneercourse.service.AccountService;
 
-@RestController
-=======
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/login")
->>>>>>> Stashed changes
 public class LoginController {
 
     private final AccountService accountService;
