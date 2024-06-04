46d5f72d4b182829630d933bc0e43fa178d31124
Van Thieu <thieudvts2208034@fpt.edu.vn>
2024-06-04 15:06:07 +0700

    update resources js, scss

--- PixelPioneerCourse/src/main/java/vn/aptech/pixelpioneercourse/config/WebConfiguration.java
+++ PixelPioneerCourse/src/main/java/vn/aptech/pixelpioneercourse/config/WebConfiguration.java
@@ -5,7 +5,6 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 public class WebConfiguration {
--- PixelPioneerCourse/src/main/java/vn/aptech/pixelpioneercourse/service/PayPalService.java
+++ PixelPioneerCourse/src/main/java/vn/aptech/pixelpioneercourse/service/PayPalService.java
@@ -3,11 +3,10 @@ package vn.aptech.pixelpioneercourse.service;
 import com.paypal.api.payments.*;
 import com.paypal.base.rest.APIContext;
 import com.paypal.base.rest.PayPalRESTException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import vn.aptech.pixelpioneercourse.dto.CreditCardDto;
 import vn.aptech.pixelpioneercourse.dto.PaymentRequestDto;
 import vn.aptech.pixelpioneercourse.entities.PaymentMethod;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,66 +18,45 @@ public class PayPalService {
     private APIContext apiContext;
 
     public Payment createPayment(PaymentRequestDto paymentRequestDto, String cancelUrl, String successUrl) throws PayPalRESTException {
-        if (paymentRequestDto.getPaymentMethod() == PaymentMethod.PAYPAL) {
-            return createPayPalPayment(paymentRequestDto.getAmount(), "USD", "paypal", "sale", "Course payment", cancelUrl, successUrl);
-        } else if (paymentRequestDto.getPaymentMethod() == PaymentMethod.CREDIT_CARD && paymentRequestDto.getCreditCard() != null) {
-            return createCreditCardPayment(paymentRequestDto.getAmount(), "USD", "sale", "Course payment", paymentRequestDto.getCreditCard());
-        } else {
-            throw new IllegalArgumentException("Invalid payment method or missing credit card information");
-        }
-    }
-
-    private Payment createPayPalPayment(Double total, String currency, String method, String intent, String description, String cancelUrl, String successUrl) throws PayPalRESTException {
         Amount amount = new Amount();
-        amount.setCurrency(currency);
-        amount.setTotal(String.format("%.2f", total));
+        amount.setCurrency("USD");
+        amount.setTotal(String.format("%.2f", paymentRequestDto.getAmount()));
 
         Transaction transaction = new Transaction();
-        transaction.setDescription(description);
+        transaction.setDescription("Course Payment");
         transaction.setAmount(amount);
 
         List<Transaction> transactions = new ArrayList<>();
         transactions.add(transaction);
 
         Payer payer = new Payer();
-        payer.setPaymentMethod(method);
+        if (paymentRequestDto.getPaymentMethod() == PaymentMethod.PAYPAL) {
+            payer.setPaymentMethod("paypal");
+        } else if (paymentRequestDto.getPaymentMethod() == PaymentMethod.CREDIT_CARD) {
+            payer.setPaymentMethod("credit_card");
+            CreditCard creditCard = new CreditCard()
+                .setNumber(paymentRequestDto.getCreditCard().getNumber())
+                .setType(paymentRequestDto.getCreditCard().getType())
+                .setExpireMonth(Integer.parseInt(paymentRequestDto.getCreditCard().getExpireMonth()))
+                .setExpireYear(Integer.parseInt(paymentRequestDto.getCreditCard().getExpireYear()))
+                .setCvv2(paymentRequestDto.getCreditCard().getCvv2())
+                .setFirstName(paymentRequestDto.getCreditCard().getFirstName())
+                .setLastName(paymentRequestDto.getCreditCard().getLastName());
+            FundingInstrument fundingInstrument = new FundingInstrument();
+            fundingInstrument.setCreditCard(creditCard);
+            List<FundingInstrument> fundingInstrumentList = new ArrayList<>();
+            fundingInstrumentList.add(fundingInstrument);
+            payer.setFundingInstruments(fundingInstrumentList);
+        }
 
         Payment payment = new Payment();
-        payment.setIntent(intent);
+        payment.setIntent("sale");
         payment.setPayer(payer);
         payment.setTransactions(transactions);
-
         RedirectUrls redirectUrls = new RedirectUrls();
         redirectUrls.setCancelUrl(cancelUrl);
         redirectUrls.setReturnUrl(successUrl);
         payment.setRedirectUrls(redirectUrls);
-
-        return payment.create(apiContext);
-    }
-
-    private Payment createCreditCardPayment(Double total, String currency, String intent, String description, CreditCardDto creditCardDto) throws PayPalRESTException {
-        Amount amount = new Amount();
-        amount.setCurrency(currency);
-        amount.setTotal(String.format("%.2f", total));
-
-        Transaction transaction = new Transaction();
-        transaction.setDescription(description);
-        transaction.setAmount(amount);
-
-        List<Transaction> transactions = new ArrayList<>();
-        transactions.add(transaction);
-
-        FundingInstrument fundingInstrument = new FundingInstrument();
-        fundingInstrument.setCreditCard(creditCardDto.toCreditCard());
-
-        Payer payer = new Payer();
-        payer.setPaymentMethod("credit_card");
-        payer.setFundingInstruments(List.of(fundingInstrument));
-
-        Payment payment = new Payment();
-        payment.setIntent(intent);
-        payment.setPayer(payer);
-        payment.setTransactions(transactions);
 
         return payment.create(apiContext);
     }
