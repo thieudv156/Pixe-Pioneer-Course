bd296edb66d6f948cfd62ec445c79fe04e67331d
Van Thieu <thieudvts2208034@fpt.edu.vn>
2024-06-21 08:21:48 +0700

    update layout

--- PixelPioneerCourse/src/main/java/vn/aptech/pixelpioneercourse/controller/app/home/HomeController.java
+++ PixelPioneerCourse/src/main/java/vn/aptech/pixelpioneercourse/controller/app/home/HomeController.java
@@ -3,6 +3,7 @@ package vn.aptech.pixelpioneercourse.controller.app.home;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
 @RequestMapping("/")
