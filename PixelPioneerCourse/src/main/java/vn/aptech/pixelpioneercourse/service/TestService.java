fb238474a1c72cc50aa651c62877c4a9520eb7de
Van Thieu <thieudvts2208034@fpt.edu.vn>
2024-06-24 14:07:38 +0700

    fix role instructor + test

--- PixelPioneerCourse/src/main/java/vn/aptech/pixelpioneercourse/service/TestService.java
+++ PixelPioneerCourse/src/main/java/vn/aptech/pixelpioneercourse/service/TestService.java
@@ -1,7 +1,9 @@
 package vn.aptech.pixelpioneercourse.service;
 
 import vn.aptech.pixelpioneercourse.dto.TestDto;
+import vn.aptech.pixelpioneercourse.entities.Test;
 
 public interface TestService {
     TestDto createTestDto(Integer testFormatId, Integer userId);
+    Test submitTest(TestDto testDto, Integer userId);
 }
